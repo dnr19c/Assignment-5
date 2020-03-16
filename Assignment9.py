f4b771d1056ae7b7ec5983187787de9fd5a5bd68
dnr19c <54766270+dnr19c@users.noreply.github.com>
2020-03-16 06:09:16 -0400

    Add files via upload

---
+++ Assignment9.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+def parse_int(s):
+    return int(s)
+
+class TestConversion(unittest.TestCase):
+    def test_bad_int(self):
+        self.assertRaises(ValueError, parse_int, 'N/A')
+        
